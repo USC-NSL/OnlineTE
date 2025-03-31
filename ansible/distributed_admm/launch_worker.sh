42a91bcc9166e0782073c04412c6a3454002ea0a
Arvin Ghavidel <arvinghavidel@gmail.com>
2025-03-30 20:59:40 -0700

    More Ansible things

--- ansible/distributed_admm/launch_worker.sh
+++ ansible/distributed_admm/launch_worker.sh
@@ -7,4 +7,16 @@ if [[ -z "${WORKER_ID}" ]]; then
   exit -1
 fi
 
-/usr/bin/python3 -m te.algorithms.formulations.edge_based_distributed_admm.worker "${WORKER_ID}"
+_term() {
+  kill -15 "$child" 2>/dev/null
+}
+_kill() {
+  kill -9 "$child" 2>/dev/null
+}
+
+trap _term SIGTERM
+trap _kill SIGKILL
+
+/usr/bin/python3 -m te.algorithms.formulations.edge_based_distributed_admm.worker "${WORKER_ID}" &
+child=$!
+wait "$child"
