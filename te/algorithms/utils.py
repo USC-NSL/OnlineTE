0458ba426bbdf65d463a8cb50f7e210f93917ad3
Arvin Ghavidel <arvinghavidel@gmail.com>
2025-09-01 21:27:54 -0700

    Fixed a small mistake

--- te/algorithms/utils.py
+++ te/algorithms/utils.py
@@ -140,7 +140,7 @@ def get_solution_confusion_matrix(lp: TrafficEngineeringLP, eval_params: Traffic
                 else:
                     fig = plt.figure(figsize=(12, 3))
                     plt.subplot(1, 3, 1)
-                    plt.xlim([100, 300])
+                    # plt.xlim([100, 300])
                     objective_trace.plot()
                     ax = plt.subplot(1, 3, 2)
                     plt.plot(objective_gap_trace)
